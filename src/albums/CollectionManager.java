dd7d048be4fbaf420a1fdbe03acaf848954b4063
Joshua Sze <joshuasze@nbp-83-6.nbp.ruw.rutgers.edu>
2021-09-20 20:16:08 -0400

    Finished methodization of CollectionManager.java

--- src/albums/CollectionManager.java
+++ src/albums/CollectionManager.java
@@ -11,6 +11,83 @@ import java.util.Scanner;
 public class CollectionManager
 {
     private static final int NOT_FOUND = 0;
+
+    private Collection CM_add(StringTokenizer st, Collection c)
+    {
+        String title = st.nextToken();
+        String artist = st.nextToken();
+        String genreType = st.nextToken();
+        if (!(genreType.equals("classical") || genreType.equals("country") || genreType.equals("jazz") || genreType.equals("pop")))
+        {
+            genreType = "unknown";
+        }
+        Genre genre = Genre.valueOf(genreType);
+        Date date = new Date(st.nextToken());
+        boolean isAvailable = true;
+        Album temp = new Album(title, artist, genre, date, isAvailable);
+        if (!date.isValid())
+        {
+            System.out.println("Invalid date!");
+            return c;
+        }
+        else if (c.add(temp))
+        {
+            System.out.println(temp.toString() + " >> added");
+            return c;
+        }
+        else
+        {
+            System.out.println(temp.toString() + " >> is already in the collection.");
+            return c;
+        }
+    }
+
+    private Collection CM_delete(StringTokenizer st, Collection c)
+    {
+        String title = st.nextToken();
+        String artist = st.nextToken();
+        Album temp = new Album(title, artist);
+        if (c.remove(temp))
+        {
+            System.out.println(title + "::" + artist + " >> deleted.");
+            return c;
+        }
+        else {
+            System.out.println(title + "::" + artist + " >> is not in the collection.");
+            return c;
+        }
+    }
+
+    private Collection CM_lendingOut(StringTokenizer st, Collection c)
+    {
+        String title = st.nextToken();
+        String artist = st.nextToken();
+        Album temp = new Album(title, artist);
+        if (c.lendingOut(temp)) {
+            System.out.println(title + "::" + artist + " >> lending out and set to not available.");
+            return c;
+        }
+        else {
+            System.out.println(title + "::" + artist + " >> is not available.");
+            return c;
+        }
+    }
+
+    private Collection CM_return(StringTokenizer st, Collection c)
+    {
+        String title = st.nextToken();
+        String artist = st.nextToken();
+        Album temp = new Album(title, artist);
+        if (c.returnAlbum(temp)) {
+            System.out.println(title + "::" + artist + " >> returning and set to available.");
+            return c;
+        }
+        else {
+            System.out.println(title + "::" + artist + " >> return cannot be completed.");
+            return c;
+        }
+    }
+
     public void run()
     {
         Scanner s = new Scanner(System.in);
@@ -22,48 +99,10 @@ public class CollectionManager
         {
             StringTokenizer st = new StringTokenizer(inputLine, ",");
             String command = st.nextToken();
-            if (command.equals("A"))
-            {
-                String title = st.nextToken();
-                String artist = st.nextToken();
-                String genreType = st.nextToken();
-                if (!(genreType.equals("classical") || genreType.equals("country") || genreType.equals("jazz") || genreType.equals("pop")))
-                {
-                    genreType = "unknown";
-                }
-                Genre genre = Genre.valueOf(genreType);
-                Date date = new Date(st.nextToken());
-                boolean isAvailable = true;
-                Album temp = new Album(title, artist, genre, date, isAvailable);
-                if (!date.isValid()) System.out.println("Invalid date!");
-                else if (c.add(temp)) System.out.println(temp.toString() + " >> added");
-                else System.out.println(temp.toString() + " >> is already in the collection.");
-
-            }
-            else if (command.equals("D"))
-            {
-                String title = st.nextToken();
-                String artist = st.nextToken();
-                Album temp = new Album(title, artist);
-                if (c.remove(temp)) System.out.println(title + "::" + artist + " >> deleted.");
-                else System.out.println(title + "::" + artist + " >> is not in the collection.");
-            }
-            else if (command.equals("L"))
-            {
-                String title = st.nextToken();
-                String artist = st.nextToken();
-                Album temp = new Album(title, artist);
-                if (c.lendingOut(temp)) System.out.println(title + "::" + artist + " >> lending out and set to not available.");
-                else System.out.println(title + "::" + artist + " >> is not available.");
-            }
-            else if (command.equals("R"))
-            {
-                String title = st.nextToken();
-                String artist = st.nextToken();
-                Album temp = new Album(title, artist);
-                if (c.returnAlbum(temp)) System.out.println(title + "::" + artist + " >> returning and set to available.");
-                else System.out.println(title + "::" + artist + " >> return cannot be completed.");
-            }
+            if (command.equals("A")) c = CM_add(st, c);
+            else if (command.equals("D")) c = CM_delete(st, c);
+            else if (command.equals("L")) c = CM_lendingOut(st, c);
+            else if (command.equals("R")) c = CM_return(st, c);
             else if (command.equals("P")) c.print();
             else if (command.equals("PD")) c.printByReleaseDate();
             else if (command.equals("PG")) c.printByGenre();
