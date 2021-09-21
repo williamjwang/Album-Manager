ce8536029b2232a6b97bf2c6a46bb437f4c888a6
Joshua Sze <joshuasze@nbp-83-6.nbp.ruw.rutgers.edu>
2021-09-20 20:19:10 -0400

    Fixed merging

--- src/albums/CollectionManager.java
+++ src/albums/CollectionManager.java
@@ -104,55 +104,10 @@ public class CollectionManager
         {
             StringTokenizer st = new StringTokenizer(inputLine, ",");
             String command = st.nextToken();
-<<<<<<< HEAD
             if (command.equals("A")) c = CM_add(st, c);
             else if (command.equals("D")) c = CM_delete(st, c);
             else if (command.equals("L")) c = CM_lendingOut(st, c);
             else if (command.equals("R")) c = CM_return(st, c);
-=======
-            if (command.equals("A"))
-            {
-                String title = st.nextToken();
-                String artist = st.nextToken();
-                String genreType = firstToUpperCase(st.nextToken());
-                if (!(genreType.equals("Classical") || genreType.equals("Country") || genreType.equals("Jazz") || genreType.equals("Pop")))
-                {
-                    genreType = "Unknown";
-                }
-                Genre genre = Genre.valueOf(genreType);
-                Date date = new Date(st.nextToken());
-                boolean isAvailable = true;
-                Album temp = new Album(title, artist, genre, date, isAvailable);
-                if (!date.isValid()) System.out.println("Invalid date!");
-                else if (c.add(temp)) System.out.println(temp.toString() + " >> added.");
-                else System.out.println(temp.toString() + " >> is already in the collection.");
-
-            }
-            else if (command.equals("D"))
-            {
-                String title = st.nextToken();
-                String artist = st.nextToken();
-                Album temp = new Album(title, artist);
-                if (c.remove(temp)) System.out.println(title + "::" + artist + " >> deleted.");
-                else System.out.println(title + "::" + artist + " >> is not in the collection.");
-            }
-            else if (command.equals("L"))
-            {
-                String title = st.nextToken();
-                String artist = st.nextToken();
-                Album temp = new Album(title, artist);
-                if (c.lendingOut(temp)) System.out.println(title + "::" + artist + " >> lending out and set to not available.");
-                else System.out.println(title + "::" + artist + " >> is not available.");
-            }
-            else if (command.equals("R"))
-            {
-                String title = st.nextToken();
-                String artist = st.nextToken();
-                Album temp = new Album(title, artist);
-                if (c.returnAlbum(temp)) System.out.println(title + "::" + artist + " >> returning and set to available.");
-                else System.out.println(title + "::" + artist + " >> return cannot be completed.");
-            }
->>>>>>> 8eef07ca7a2649640f2cbf4f4d3c2fba5228eb1d
             else if (command.equals("P")) c.print();
             else if (command.equals("PD")) c.printByReleaseDate();
             else if (command.equals("PG")) c.printByGenre();
