b5c73a777e6afcbf82aa57cc10e400dfd2697410
Joshua Sze <62036406+Josephua@users.noreply.github.com>
2021-09-22 17:14:03 -0400

    Update RunProject1.java

--- src/albums/RunProject1.java
+++ src/albums/RunProject1.java
@@ -1,5 +1,12 @@
 package albums;
 
+/**
+ * This class runs the entire project via the RunProject1 class through the main method. 
+ * It calls the run method in the CollectionManager.
+ *
+ * @author William Wang, Joshua Sze
+ */
+
 public class RunProject1
 {
     public static void main(String[] args)
